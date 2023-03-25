4b8a8ce4d160ec95d292d670c96a8f648574e09d
John Truong <viajohnt@gmail.com>
2023-03-25 15:50:35 -0600

    final commit

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(response => response.json())
+  .then(books => renderBooks(books));
+    
 }
+ 
+
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
@@ -15,3 +21,4 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+
